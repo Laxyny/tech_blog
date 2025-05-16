a1059360d5c3ccaf573daa43254e39ad4cd2590e
Laxyny <kevgreg30@gmail.com>
2025-05-16 13:28:53 +0200

    *END*

---
+++ src/Controller/PostController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Entity\Comment;
+use App\Form\CommentFormTypeForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PostController extends AbstractController
+{
+    #[Route('/post/{id}', name: 'app_post_show')]
+    public function show(Post $post, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormTypeForm::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setPost($post);
+            $comment->setCreatedAt(new \DateTimeImmutable());
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_show', ['id' => $post->getId()]);
+        }
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+}
